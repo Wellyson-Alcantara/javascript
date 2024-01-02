42a89241fc7f973ddd695b7773945be984457371
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-01-02 14:36:16 -0300

    criei a pasta ex009 o index, o scrip.js e o style

---
+++ exercicios/ex009/script.js
@@ -0,0 +1,28 @@
+/*
+    ## IMPORTANTE ##
+    Você tem todo o direito de usar esse material 
+    para seu próprio aprendizado. Professores também 
+    podem ter acesso a todo o conteúdo e usá-los com 
+    seus alunos. Porém todos os que usarem esse 
+    material - seja para qual for a finalidade - deverão 
+    manter a referência ao material original, disponível 
+    em https://github.com/gustavoguanabara/javascript. Este 
+    material não poderá ser utilizado em nenhuma hipótese 
+    para ser replicada - integral ou parcialmente - 
+    por autores/editoras/instituições para criar livros 
+    ou apostilas, com finalidades de obter ganho financeiro 
+    com ele.
+*/
+
+let contador = 0
+let res = document.querySelector('section#resultado')
+
+function contar() {
+    contador ++
+    res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
+}
+
+function zerar() {
+    contador = 0
+    res.innerHTML = `<p>O contador está com <mark>0</mark> cliques.</p>`
+}
