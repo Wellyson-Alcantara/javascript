a420ce31fb9d4403c897225968b0aaff0be0c9e2
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-01-07 14:10:58 -0300

    criei a pasta ex010, o index, o arquivo js e o style

---
+++ exercicios/ex010/açoes.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida')
+
+function açao1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function açao2() {
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function açao3() {
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function açao4() {
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
