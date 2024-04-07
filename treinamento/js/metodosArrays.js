66e9b189396100ffb5f94469c406d3e32eb1b1d5
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-04-06 21:45:50 -0300

    criei o metodos arrays

---
+++ treinamento/js/metodosArrays.js
@@ -0,0 +1,31 @@
+/* Criando Arrays Recaptulação 
+
+const meuArrayLiteral = [1, 2, 3] // Forma literal
+
+const meuArrayConstructorInstance = new Array(1, 2, 3)
+
+const meuArrayConstructorCall = Array(1, 2, 3)
+
+const meuArrayFrom = Array.from(meuArrayLiteral)
+
+const meuArraySpread = [...meuArrayFrom]
+
+""
+
+*/
+
+/* INSERIR E REMOVER ELEMENTOS DAS EXTREMIDADES 
+
+const arr = [1]
+
+arr.push(2)
+
+arr.unshift(10)
+
+arr.shift()
+
+arr.pop()
+
+""
+
+*/
