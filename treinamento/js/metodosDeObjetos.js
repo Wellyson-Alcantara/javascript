09573a1c848b438af5539dde82a228cad59935b9
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-04-08 19:24:09 -0300

    criei o metodo de objetos

---
+++ treinamento/js/metodosDeObjetos.js
@@ -0,0 +1,118 @@
+/*
+const destino = { a: 1, b: 2}
+const origem = { b: 4, c: 5}
+
+const destinoRetornado = Object.assign(destino, origem)
+
+console.log(destinoRetornado, destino)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const obj = {a: 1}
+const copia = Object.assign({}, obj)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const o1 = {a: 1}
+const o2 = {b: 2}
+const o3 = {c: 3}
+
+const obj = Object.assign({}, o1, o2, o3) // Quando é colocado Chaves{} dentro dos parenteses não acontererá a mesclagem do primeiro elemento no começo do index, caso contrário ficaria assim: o1 = a: 1, b: 2, c: 3 | o2 = b: 2 | o3 = c: 3.
+
+console.log(obj, o1)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const objetoDeEntrada = {
+    'abc': 2,
+    'def': 4
+}
+
+const array = Object.entries(objetoDeEntrada)
+
+console.log(array)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const obj = {100: 'a', 2: 'b', 7: 'c'}
+
+const keys = Object.keys(obj)
+const values = Object.values(obj)
+
+
+console.log(keys, values)
+
+*/
