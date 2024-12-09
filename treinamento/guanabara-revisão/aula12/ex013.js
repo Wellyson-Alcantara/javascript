e3428690873d242bbee9d5bc3da2029f865c40a7
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-12-09 12:23:53 -0300

    condições ramificadas e swift

---
+++ treinamento/guanabara-revisão/aula12/ex013.js
@@ -0,0 +1,12 @@
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >= 0 && hora < 6) {
+    console.log('Boa Madrugada!')
+} else if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
