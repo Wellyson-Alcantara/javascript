ec345f6eac17d0512a81dd5aa72686290fa9c8c9
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-12-18 13:51:14 -0300

    estruturas de repetição

---
+++ treinamento/guanabara-revisão/aula13/ambiente.js
@@ -0,0 +1,25 @@
+let c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+
+
+/*
+let c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
