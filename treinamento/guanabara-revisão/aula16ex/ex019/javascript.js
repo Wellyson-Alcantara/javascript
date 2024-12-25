d8321b6e8de69caaf0dc476d1719ebd5402099db
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2024-12-25 20:54:09 -0300

    construindo o meu código

--- treinamento/guanabara-revisão/aula16ex/ex019/javascript.js
+++ treinamento/guanabara-revisão/aula16ex/ex019/javascript.js
@@ -1,13 +1,28 @@
 let num = document.getElementById('txtn')
-let tab = document.getElementById('seltab')
+let lista = document.getElementById('flista')
 let res = document.getElementById('res')
 let valores = []
 
 function isNumero(n) {
-    if (Number(n) >= 1 && Number(n) >= 100) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
     }
 }
 
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
+}
